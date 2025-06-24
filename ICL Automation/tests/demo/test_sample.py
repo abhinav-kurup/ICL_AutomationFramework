9eb4d797cb6f410781f4705a93e359a55c01a137
abhinav-kurup <abhinavkurup2002@gmail.com.com>
2025-06-24 18:50:23 +0530

    Updated test file

--- ICL Automation/tests/demo/test_sample.py
+++ ICL Automation/tests/demo/test_sample.py
@@ -1,7 +1,16 @@
 import pytest
 import allure
 
-@allure.title("Sample Test")
+@allure.title("Positive Test: Always Passes")
 def test_sample():
     assert 1 == 1
+
+@allure.title("Positive Test: Sum Check")
+def test_sum():
+    result = 2 + 3
+    assert result == 5, "Expected sum to be 5"
+
+@allure.title("Negative Test: Failing Assertion")
+def test_failure():
+    assert False
 
